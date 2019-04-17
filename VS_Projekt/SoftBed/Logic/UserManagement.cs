832698ebb85fc962c892dd373b5ed4bae13fff6e
Hannes Huber <hannes_huber@t-online.de>
2019-04-17 10:56:29 +0200

    erster Test läuft

--- VS_Projekt/SoftBed/Logic/UserManagement.cs
+++ VS_Projekt/SoftBed/Logic/UserManagement.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Wrapperklassen;
 
 namespace Logic
 {
-    class UserManagement : LogicController
+    public class UserManagement : LogicController
     {
-        private static UserManagement _instance = null; 
+        private static UserManagement _instance = null;
+        private static User _currentUser;
+
+        public static User CurrentUser { get => _currentUser; set => _currentUser = value; }
+
         private UserManagement()
         {
         }
@@ -19,6 +24,27 @@ namespace Logic
             }
             return _instance;
         }
+
+        public bool UserAnlegen(User newUser)
+        {
+            return false;
+        }
+
+        public bool UserLöschen(string userName)
+        {
+            return false;
+        }
+
+        public bool UserAendern(string userName, User newUser)
+        {
+            return false;
+        }
+
+        public bool UserLogin(string userName, string password)
+        {
+            return false;
+        }
+
 
     }
 }
---
+++ VS_Projekt/SoftBed/UnitTests/UserManagementTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Logic;
+using Wrapperklassen;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UserManagementTest
+    {
+        [TestMethod]
+        public void UserAnlegenTest()
+        {
+            UserManagement userManagement = UserManagement.GetInstance();
+            User dummy = new User("John", "Doe", "Standard", "JonnyBoy", "1234");
+
+            bool firstTime = userManagement.UserAnlegen(dummy);     //sollte klappen
+            bool secondTime = userManagement.UserAnlegen(dummy);    //sollte nicht klappen
+
+            Assert.IsTrue(firstTime && !secondTime);    //erste mal muss geklappt haben, zweite mal darf nicht geklappt haben
+        }
+
+        [TestMethod]
+        public void UserLoeschenTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void UserAendernTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void UserLoginTest()
+        {
+
+        }
+    }
+}
