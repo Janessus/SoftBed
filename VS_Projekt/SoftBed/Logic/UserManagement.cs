2757d24f5342075089844003d5c45d946b7a5f54
Hannes Huber <hannes_huber@t-online.de>
2019-04-19 11:23:59 +0200

    Tests für UserManagement erledigt

--- VS_Projekt/SoftBed/Logic/UserManagement.cs
+++ VS_Projekt/SoftBed/Logic/UserManagement.cs
@@ -14,6 +14,7 @@ namespace Logic
 
         private UserManagement()
         {
+            CurrentUser = new User("Dummy", "Dummy", "Dummy", "Dummy", "Dummy");
         }
 
         public static UserManagement GetInstance()
@@ -25,26 +26,54 @@ namespace Logic
             return _instance;
         }
 
+        /**
+         * funktion zum Anlegen eines Users
+         * @param bekommt einen neuen User übergeben
+         * @return true falls geklappt oder false falls nicht möglich
+         */
         public bool UserAnlegen(User newUser)
         {
             return false;
         }
 
+        /**
+         * funktion zum löschen eines Users
+         * @param bekommt Benutzername des zu löschenden Users übergeben
+         * @return true falls geklappt und false falls nicht möglich
+         */
         public bool UserLöschen(string userName)
         {
             return false;
         }
 
+        /**
+         * funktion zum bearbeiten eines Users
+         * @param Benutzername des alten Users
+         * @param neuer User mit dem er überschrieben werden soll
+         * @return true falls geklappt, false falls nicht möglich
+         */
         public bool UserAendern(string userName, User newUser)
         {
             return false;
         }
 
+        /**
+         * funktion zum login eines Users
+         * @param Benutzername und Passwort des einloggers
+         * @return true falls geklappt, false falls nicht möglich 
+         */
         public bool UserLogin(string userName, string password)
         {
             return false;
         }
 
+        /**
+         * funktion zum logout einer Users
+         */
+         public void UserLogout()
+        {
+            CurrentUser = new User("Dummy", "Dummy", "Dummy", "Dummy", "Dummy");
+        }
 
     }
 }
--- VS_Projekt/SoftBed/UnitTests/UserManagementTest.cs
+++ VS_Projekt/SoftBed/UnitTests/UserManagementTest.cs
@@ -8,34 +8,107 @@ namespace UnitTests
     [TestClass]
     public class UserManagementTest
     {
+    /**  Hinweise:
+     * Eigentlich müsste man noch die Rechte abtesten.
+     * Da die Funktionen aber eh nur aus dem Adminbereich aufgerufen werden können ist es nicht nötig
+     */
+
+        /**
+         * Test zum User Anlegen
+         * schlägt fehl wenn User mit gleichem Benutzername zweimal angelegt werden kann
+         */
         [TestMethod]
         public void UserAnlegenTest()
         {
             UserManagement userManagement = UserManagement.GetInstance();
             User dummy = new User("John", "Doe", "Standard", "JonnyBoy", "1234");
+            User dummy2 = new User("Bat", "Man", "Azubi", "JonnyBoy", "Batman");
 
-            bool firstTime = userManagement.UserAnlegen(dummy);     //sollte klappen
-            bool secondTime = userManagement.UserAnlegen(dummy);    //sollte nicht klappen
+            bool firstTime = userManagement.UserAnlegen(dummy);     //sollte klappen da neuer Benutzername
+            bool secondTime = userManagement.UserAnlegen(dummy2);    //sollte nicht klappen da gleicher Benutzername wie zuvor
 
             Assert.IsTrue(firstTime && !secondTime);    //erste mal muss geklappt haben, zweite mal darf nicht geklappt haben
         }
 
+        /**
+         * Test zum User Löschen
+         * schlägt fehl wenn der selbe User nach dem löschen nochmal gelöscht werden kann (also nicht gelöscht wurde)
+         */
         [TestMethod]
         public void UserLoeschenTest()
         {
+            UserManagement userManagement = UserManagement.GetInstance();
+            User dummy = new User("John", "Doe", "Standard", "JonnyBoy", "1234");
+            userManagement.UserAnlegen(dummy);
 
+            bool firstTime = userManagement.UserLöschen(dummy.Benutzername);    //sollte klappen da User existent
+
+            bool secondTime = userManagement.UserLöschen(dummy.Benutzername);   //sollte fehlschlagen da User nicht mehr existent
+
+            Assert.IsTrue(firstTime && !secondTime);
         }
 
+        /**
+         * Test zum User Ändern
+         * schlägt fehl wenn User nicht angelegt werden kann oder nicht existenter User geändert werden kann
+         */
         [TestMethod]
         public void UserAendernTest()
         {
+            UserManagement userManagement = UserManagement.GetInstance();
+            User dummy = new User("John", "Doe", "Standard", "JonnyBoy", "1234");
+            User dummy2 = new User("Bat", "Man", "Azubi", "Bat42", "Batman");
+            userManagement.UserAnlegen(dummy);
+
+            bool firstTime = userManagement.UserAendern(dummy.Benutzername, dummy2);    //klappt
+
+            bool secondTime = userManagement.UserAendern("nope", dummy2);   //klappt nicht, da User nicht existiert
+
+            Assert.IsTrue(firstTime && !secondTime);
+
 
         }
 
+        /**
+         * Test zum Login
+         * schlägt fehl wenn gewollter User nicht dem eingeloggten User entspricht
+         */
         [TestMethod]
         public void UserLoginTest()
         {
+            UserManagement userManagement = UserManagement.GetInstance();
+            User dummy = new User("John", "Doe", "Standard", "JonnyBoy", "1234");
+            userManagement.UserAnlegen(dummy);
 
+            userManagement.UserLogin(dummy.Benutzername, dummy.Passwort);   
+            User loggedin = UserManagement.CurrentUser;
+
+            bool firstTime = loggedin.Benutzername.Equals(dummy.Benutzername);  //klappt, da currentUser dem zuletzt eingeloggten user entspricht
+
+            Assert.IsTrue(firstTime);
         }
+
+        /**
+        * Test zum Logout
+        * schlägt fehl wenn am anfang oder nach dem ausloggen nicht der CurrentUser dem dummy entspricht
+        */
+        [TestMethod]
+        public void UserLogoutTest()
+        {
+            UserManagement userManagement = UserManagement.GetInstance();
+            User dummy = new User("Dummy", "Dummy", "Dummy", "Dummy", "Dummy");
+            User dummy2 = new User("John", "Doe", "Standard", "JonnyBoy", "1234");
+            userManagement.UserAnlegen(dummy2);
+
+            bool firstTime = UserManagement.CurrentUser.Benutzername.Equals(dummy.Benutzername);
+
+            userManagement.UserLogin(dummy2.Benutzername, dummy2.Passwort);
+            userManagement.UserLogout();
+
+            bool secondTime = UserManagement.CurrentUser.Benutzername.Equals(dummy.Benutzername);
+
+            Assert.IsTrue(firstTime && secondTime);
+        }
+
     }
 }
