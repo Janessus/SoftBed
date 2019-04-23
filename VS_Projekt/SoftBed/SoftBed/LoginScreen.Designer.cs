14902a961698f6ee8d662d23cf34ed00565a1b92
Julia <julia.merettig@web.de>
2019-04-23 10:42:52 +0200

    LoginScreen created

---
+++ VS_Projekt/SoftBed/SoftBed/LoginScreen.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace SoftBed
+{
+    partial class LoginScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginScreen));
+            this.loginLbl = new System.Windows.Forms.Label();
+            this.userTxt = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.logoBox = new System.Windows.Forms.PictureBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.logoBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loginLbl
+            // 
+            this.loginLbl.AutoSize = true;
+            this.loginLbl.Font = new System.Drawing.Font("Adobe Fan Heiti Std B", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.loginLbl.Location = new System.Drawing.Point(320, 258);
+            this.loginLbl.Name = "loginLbl";
+            this.loginLbl.Size = new System.Drawing.Size(89, 36);
+            this.loginLbl.TabIndex = 0;
+            this.loginLbl.Text = "Login";
+            // 
+            // userTxt
+            // 
+            this.userTxt.Font = new System.Drawing.Font("Adobe Fan Heiti Std B", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userTxt.Location = new System.Drawing.Point(326, 312);
+            this.userTxt.Name = "userTxt";
+            this.userTxt.Size = new System.Drawing.Size(137, 29);
+            this.userTxt.TabIndex = 1;
+            this.userTxt.Text = "User";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Adobe Fan Heiti Std B", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(325, 347);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(138, 29);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "abcdefghijk";
+            // 
+            // logoBox
+            // 
+            this.logoBox.Image = ((System.Drawing.Image)(resources.GetObject("logoBox.Image")));
+            this.logoBox.Location = new System.Drawing.Point(253, 35);
+            this.logoBox.Name = "logoBox";
+            this.logoBox.Size = new System.Drawing.Size(277, 185);
+            this.logoBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logoBox.TabIndex = 3;
+            this.logoBox.TabStop = false;
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(167)))), ((int)(((byte)(209)))), ((int)(((byte)(79)))));
+            this.loginBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(162)))), ((int)(((byte)(62)))));
+            this.loginBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(162)))), ((int)(((byte)(62)))));
+            this.loginBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(162)))), ((int)(((byte)(62)))));
+            this.loginBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.loginBtn.Font = new System.Drawing.Font("Adobe Fan Heiti Std B", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginBtn.Location = new System.Drawing.Point(325, 387);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(138, 36);
+            this.loginBtn.TabIndex = 4;
+            this.loginBtn.Text = "login";
+            this.loginBtn.UseVisualStyleBackColor = false;
+            // 
+            // LoginScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.logoBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.userTxt);
+            this.Controls.Add(this.loginLbl);
+            this.Name = "LoginScreen";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.logoBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label loginLbl;
+        private System.Windows.Forms.TextBox userTxt;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox logoBox;
+        private System.Windows.Forms.Button loginBtn;
+    }
+}
+
--- VS_Projekt/SoftBed/SoftBed/LoginScreen.cs
+++ VS_Projekt/SoftBed/SoftBed/LoginScreen.cs
@@ -10,11 +10,12 @@ using System.Windows.Forms;
 
 namespace SoftBed
 {
-    public partial class Form1 : Form
+    public partial class LoginScreen : Form
     {
-        public Form1()
+        public LoginScreen()
         {
             InitializeComponent();
         }
+        
     }
 }
