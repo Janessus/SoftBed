658738aed85100002c2ff90e06841e46d83312f4
Hannes Huber <hannes_huber@t-online.de>
2019-05-08 11:21:46 +0200

    zeug

--- VS_Projekt/SoftBed/SoftBed/HauptFenster.cs
+++ VS_Projekt/SoftBed/SoftBed/HauptFenster.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Logic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Wrapperklassen;
 
 namespace SoftBed
 {
@@ -17,6 +20,14 @@ namespace SoftBed
             InitializeComponent();
             this.StartPosition = FormStartPosition.Manual;
             this.Location = new Point(100, 100);
+            Task refreshTask = new Task(()=>{
+                while (true)
+                {
+                    //RefreshHauptfenster();
+                    Thread.Sleep(30000);
+                }
+            });
+            refreshTask.Start();
         }
 
         private void patVerwBtn_Click(object sender, EventArgs e)
@@ -26,6 +37,11 @@ namespace SoftBed
 
         private void verlAugefBtn_Click(object sender, EventArgs e)
         {
+            DataGridViewSelectedRowCollection selectedRows = transferListeDGV.SelectedRows;
+            for(int i = 0; i < selectedRows.Count; i++)
+            {
+                ZimmerManagement.GetInstance().DeleteMemberTransferliste(selectedRows[i].Cells[0].Value.ToString(),selectedRows[i].Cells[1].Value.ToString());  //klappt vielleicht
+            }
 
         }
 
@@ -44,6 +60,33 @@ namespace SoftBed
             Form patientenVerwaltung = new PatientenVerwaltung();
             patientenVerwaltung.ShowDialog();
             this.SetVisibleCore(true);
+        }
+
+        private void RefreshHauptfenster()
+        {
+            RefreshBettenbelegung();
+            RefreshVerlegungsliste();
+        }
+
+        private void RefreshBettenbelegung()
+        {
+            Bettenbelegung currentBelegung = UpdateManagement.GetInstance().GetCurrentBettenbelegung();
+            gynProgBar.Value = currentBelegung.Gynaekologie;
+            iMProgBar.Value = currentBelegung.Innere;
+            onkProgBar.Value = currentBelegung.Onkologie;
+            orthProgBar.Value = currentBelegung.Orthopaedie;
+            paedProgBar.Value = currentBelegung.Paediatrie;
+            itsProgBar.Value = currentBelegung.Intensiv;
+            gesKHProgBar.Value = currentBelegung.Gesamt();
+        }
+
+        private void RefreshVerlegungsliste()
+        {
+            Verlegungsliste currentVerlegungsliste = UpdateManagement.GetInstance().GetCurrentVerlegungsliste();
+            for(int i = 0; i < currentVerlegungsliste.Transferliste.Count; i++)
+            {
+                transferListeDGV.Rows.Add(currentVerlegungsliste.Transferliste[i]);     //klappt vielleicht
+            }
         }
     }
 }
