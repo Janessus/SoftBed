5631e4b30c22527f57558b1440ca86c81141f37d
Janessus <janesheuberger@web.de>
2019-05-08 11:40:52 +0200

    TestDaten

---
+++ Datenbank/TestDaten.sql
@@ -0,0 +1,2 @@
+INSERT INTO Station(Bezeichnung) VALUES("Innere Medizin");
+INSERT INTO Person(Vorname, Nachname, Adresse, Geschlecht, Geburtsdatum) VALUES("Janes", "Heuberger", "Schutterwald", "M", DATE("1994-09-01"));
