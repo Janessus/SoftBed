5f1699d55ea2be9f1981f7a6ff9a28dac149353a
Janessus <janesheuberger@web.de>
2019-05-08 12:27:56 +0100

    stash ausprobiert

---
+++ Datenbank/TestDaten.sql
@@ -0,0 +1,3 @@
+INSERT INTO Station(Bezeichnung) VALUES("Innere Medizin");
+INSERT INTO Person(Vorname, Nachname, Adresse, Geschlecht, Geburtsdatum) VALUES("Janes", "Heuberger", "Schutterwald", "M", DATE("1994-09-01"));
+test
