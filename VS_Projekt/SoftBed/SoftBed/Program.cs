b70c381a66d8094739713a0e25bab5acbab21055
Hannes Huber <hannes_huber@t-online.de>
2019-06-20 17:17:40 +0200

    VERSION 1.0 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- VS_Projekt/SoftBed/SoftBed/Program.cs
+++ VS_Projekt/SoftBed/SoftBed/Program.cs
@@ -9,7 +9,7 @@ namespace SoftBed
 {
     static class Program
     {
-        /// <summary>
+        /// <summary> 
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
